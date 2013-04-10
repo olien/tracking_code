5adc0332b8cfdfe8922695d70bf207a3589e3214
RexDude <rexdude@arcor.de>
2013-04-10 21:58:15 +0200

    Better init() when db is not available

--- classes/class.rex_tracking_code.inc.php
+++ classes/class.rex_tracking_code.inc.php
@@ -8,7 +8,9 @@ class rex_tracking_code {
 		$sql = new rex_sql();
 		$sql->setQuery('SELECT * FROM ' . $REX['TABLE_PREFIX'] . 'tracking_code WHERE id = 1');
 		
-		self::$trackingCode = $sql->getValue('tracking_code');
+		if ($sql->getRows() > 0) {
+			self::$trackingCode = $sql->getValue('tracking_code');
+		}
 	}
 
 	public static function getTrackingCode() {
